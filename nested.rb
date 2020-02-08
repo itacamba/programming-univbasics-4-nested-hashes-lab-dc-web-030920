f31e0e21dbd0952d443851312f84bde0ab97b2ee
Carla D Sahagun <daniela.sahagun@hotmail.com>
2020-02-08 04:30:13 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -128,7 +128,7 @@ def adding_to_dennis
         }
      }
 
-		programmer_hash[:dennis_ritchie][:languages].merge("Assembly")
+		programmer_hash[:dennis_ritchie][:languages] << "Assembly"
 		p programmer_hash
 
 end
